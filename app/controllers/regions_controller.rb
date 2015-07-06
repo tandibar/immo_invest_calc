2f34bd899f8da0447b22de14b20223f9c301264f
Prelang Builder <builder@prelang.com>
2015-07-06 19:18:06 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,4 +1,8 @@
 class RegionsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_region, only: [:show, :edit, :update, :destroy]
 
   # GET /regions
